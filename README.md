81d7e0ee99ee7373709f2ea1ca816ff788c69a42
Martin <github@amusingtime.uk>
2022-09-19 08:05:11 +0100

    Add migratePDFoundry() global function

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 ![Foundry Info](https://img.shields.io/badge/Foundry-v10-informational)
 ![Latest Release Download Count](https://img.shields.io/github/downloads/farling42/fvtt-pdf-pager/latest/module.zip)
 
-
 # PDF Pager
 
 This module provides the following:
@@ -17,7 +16,7 @@ This module provides the following:
 
 ## Example
 
-```
+```text
 @UUID{JournalEntry.T29aMDmLCPYybApI.JournalEntryPage.iYV6uMnFwdgZORxi#page=10}
 ```
 
@@ -25,6 +24,21 @@ This module provides the following:
 
 The module is available from the Foundry Module Management window, just search for "PDF Pager", or it can be manually added with the following link:
 
+[https://github.com/farling42/fvtt-pdf-pager/releases/latest/download/module.json]
+
+## Migrating from PDFoundry
+
+A function is available which can be called directly from a macro script or from the console command-line.
+
+It takes a single optional parameter which is an object that can contain the single boolean field 'onlyIfEmpty', which if set will only migrate PDFoundry
+journal entries which currently have no pages.
+
+```js
+migratePDFoundry()
 ```
-https://github.com/farling42/fvtt-pdf-pager/releases/latest/download/module.json
+
+OR
+
+```js
+migratePDFoundry({onlyIfEmpty:true})
 ```
--- scripts/pdf-pager.mjs
+++ scripts/pdf-pager.mjs
@@ -1,3 +1,26 @@
+/*
+PDF-PAGER
+
+Copyright © 2022 Martin Smith
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without 
+restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+*/
+
 const MODULE_NAME="pdf-pager";
 const CONFIG_ALWAYS_LOAD_PDF="alwaysLoadPdf";
 const FLAG_PAGE_OFFSET="pageOffset";
@@ -28,12 +51,13 @@ Hooks.once('init', () => {
 		default: true,
 		config: true
 	});
+    globalThis.migratePDFoundry = migratePDFoundry;
 });
 
-Hooks.once('ready', () => {
+Hooks.once('ready', async () => {
     // Need to capture the PDF page number
     libWrapper.register(MODULE_NAME, 'JournalSheet.prototype._render', my_journal_render, libWrapper.WRAPPER);
-    migratePDFoundry();
+    await migratePDFoundry({onlyIfEmpty:true});
 });
 
 // Ugly hack to get the PAGE number from the JournalSheet#render down to the JournalPDFPageSheet#render
@@ -91,20 +115,31 @@ Hooks.on("renderJournalPDFPageSheet", async function(sheet, html, data) {
  * Convert existing journal entries from PDFoundry to PDF-Pager format
  */
 
-export default async function migratePDFoundry() {
-    console.log(`Starting migratePDFoundry`)
-    for (const entry of game.journal) {
-        if (entry.pages.size>0) continue;  // Presumably we've already converted it
+async function migratePDFoundry(options={}) {
+    async function migrateOne(entry, options) {
+        if (options.onlyIfEmpty && entry.pages.size>0) return;  // Presumably we've already converted it
         let pdfdata = entry.flags?.pdfoundry?.PDFData;
-        if (!pdfdata) continue;  // There is no PDFoundry information in this page.
+        if (!pdfdata) return;  // There is no PDFoundry information in this page.
 
         await entry.createEmbeddedDocuments("JournalEntryPage", [{
             name:  pdfdata.name || entry.name,
             type:  "pdf",
-            flags: { "pdf-pager" : { pageOffset: pdfdata.offset }},
+            flags: { [MODULE_NAME]: { [FLAG_PAGE_OFFSET]: pdfdata.offset }},
             src:   pdfdata.url
          }]);
-         console.log(`Migrated ${pdfdata.name} to new PDF format`);
+         console.log(`Migrated '${pdfdata.name||entry.name}' to new PDF format`);
+    }
+
+    console.log(`Starting migratePDFoundry`)
+    for (const entry of game.journal) {
+        await migrateOne(entry, options);
+    }
+    for (const pack of game.packs) {
+        if (pack.locked || pack.metadata.type != 'JournalEntry') continue;
+        console.log(`Migrating JournalEntry pack '${pack.metadata.label}'`)
+        for (const elem of pack.index) {
+            await migrateOne(await pack.getDocument(elem._id), options);
+        }
     }
     console.log(`Finished migratePDFoundry`)
 }
